4e5dc52710fb27d95f1a5b73d81e2a318b22af75
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-07-10 00:32:09 -0300

    aula 02 concluida

---
+++ cmd/journey/journey.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"journey/internal/api"
+	"journey/internal/api/spec"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"github.com/phenpessoa/gutils/netutils/httputils"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGKILL)
+	defer cancel()
+
+	if err := run(ctx); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("goodbye :)")
+}
+
+func run(ctx context.Context) error {
+	cfg := zap.NewDevelopmentConfig()
+	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+
+	logger, err := cfg.Build()
+	if err != nil {
+		return err
+	}
+
+	logger = logger.Named("journey_app")
+	defer func() { _ = logger.Sync() }()
+
+	// Configuração do banco de dados a partir das variáveis de ambiente
+	host := os.Getenv("JOURNEY_DATABASE_HOST")
+	port := os.Getenv("JOURNEY_DATABASE_PORT")
+	database := os.Getenv("JOURNEY_DATABASE_NAME")
+	user := os.Getenv("JOURNEY_DATABASE_USER")
+	password := os.Getenv("JOURNEY_DATABASE_PASSWORD")
+
+	pool, err := pgxpool.New(ctx, fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s",
+		user,
+		password,
+		host,
+		port,
+		database,
+	))
+	if err != nil {
+		return err
+	}
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		return err
+	}
+
+	r := chi.NewMux()
+	r.Use(middleware.RequestID, middleware.Recoverer, httputils.ChiLogger(logger))
+
+	si := api.NewApi(
+		pool,
+		logger,
+	)
+
+	r.Mount("/", spec.Handler(&si))
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      r,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	defer func() {
+		const timeout = 30 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			logger.Error("failed to shutdown server", zap.Error(err))
+		}
+	}()
+
+	errChan := make(chan error, 1)
+
+	go func() {
+		fmt.Println("server starting on:", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil {
+			errChan <- err
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		return nil
+	case err := <-errChan:
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return err
+		}
+	}
+
+	return nil
+}
